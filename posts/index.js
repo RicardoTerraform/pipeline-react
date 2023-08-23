0cfed626e6373716993304fd31eb2d5bff4e9c35
Ricardo Jorge Alves <ricardo.j.alves@noesis.pt>
2023-08-23 09:11:16 +0100

    upload projects

---
+++ posts/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//store all posts we create
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
